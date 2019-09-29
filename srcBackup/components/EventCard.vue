cf1d5a61770a5c6fe57dc99cf661fd1bf827ae20
Tao Jiang <jiangtaotao55@yahoo.com>
2019-09-29 10:53:29 -0400

    Initial commit

---
+++ srcBackup/components/EventCard.vue
@@ -0,0 +1,23 @@
+<!-- <template>
+<h1 v-if="" @click="handler(arg, event)"></h1>
+</template> -->
+
+<script>
+//import { libName } from 'libName'
+
+export default {
+  props: {
+    propName: {
+      type: Number,
+      default: 1
+    }
+  },
+  data() {
+    return {
+      // key: value
+    }
+  }
+}
+</script>
+
+<style></style>
